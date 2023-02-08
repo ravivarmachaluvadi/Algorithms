1d7a3e80a1cbf61c9408517c2035f88d62b7a4d0
Ravi Varma Chaluvadi <58871650+ravivarmachaluvadi@users.noreply.github.com>
2023-02-09 03:56:17 +0530

    added 8

--- src/main/java/com/example/techiedelight/Algorithms/git/Main.java
+++ src/main/java/com/example/techiedelight/Algorithms/git/Main.java
@@ -2,6 +2,7 @@ package com.example.techiedelight.Algorithms.git;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(8);
         System.out.println(3);
         System.out.println(4);
     }
