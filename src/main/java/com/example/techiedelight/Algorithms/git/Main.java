1718c7f049c4f4c96a41abbcce85082ee9a3d64f
ravivarmachaluvadi <ravivarmachaluvadi@gmail.com>
2023-02-09 04:24:29 +0530

    m1

--- src/main/java/com/example/techiedelight/Algorithms/git/Main.java
+++ src/main/java/com/example/techiedelight/Algorithms/git/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(5);
         System.out.println(6);
-        System.out.println(7);
+        System.out.println(8);
+    }
+
+    public void m1() {
+        System.out.println("m1");
     }
 }
