b8f565b320390a1dfbe6c665baa5d6cbcc8f0103
ravivarmachaluvadi <ravivarmachaluvadi@gmail.com>
2023-02-09 03:41:13 +0530

    56

--- src/main/java/com/example/techiedelight/Algorithms/git/Main.java
+++ src/main/java/com/example/techiedelight/Algorithms/git/Main.java
@@ -2,7 +2,7 @@ package com.example.techiedelight.Algorithms.git;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
+        System.out.println(5);
+        System.out.println(6);
     }
 }
